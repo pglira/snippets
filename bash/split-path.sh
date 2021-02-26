023411089920aa6b10d0eeb9475607a3219ea9aa
Philipp Glira <philipp.glira@ait.ac.at>
2021-02-26 12:53:34 +0100

    Added example path

--- bash/split-path.sh
+++ bash/split-path.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+
+filepath="/tmp/dir1/dir2/file.sh"
+
 dir=$(dirname "${filepath}")
-filename=$(basename -- "${filepath}")
-extension="${filename##*.}"
-filename="${filename%.*}"
+filename_without_dir=$(basename -- "${filepath}")
+extension="${filename_without_dir##*.}"
+filename_without_extension="${filename_without_dir%.*}"
+
+echo "filepath = ${filepath}"
+echo "dir = ${dir}"
+echo "filename_without_dir = ${filename_without_dir}"
+echo "extension = ${extension}"
+echo "filename_without_extension = ${filename_without_extension}"
